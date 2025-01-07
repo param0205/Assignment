979a61acac0dff699398f79342ad5a7a8bf97bfb
param0205 <param020599@gmail.com>
2025-01-07 17:36:36 +0530

    Deploy fixed MediaQureychanges

--- Assessment01/src/hooks/UseMediaQuery.jsx
+++ Assessment01/src/hooks/UseMediaQuery.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState ,useEffect } from 'react'
 
-const useMediaQuery = (query) => {
+const UseMediaQuery = (query) => {
    
     const [matches, setMatches] = useState(false);
 
@@ -18,4 +18,4 @@ const useMediaQuery = (query) => {
   return matches;
 }
 
-export default useMediaQuery
+export default UseMediaQuery
